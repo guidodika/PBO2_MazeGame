3fb525630288ded6ec370be1826b51a242c0bda5
My Computer <My Computer@MyComputer-PC>
2018-11-15 14:28:49 +0700

    MazeGame 0.2

--- MazeGame/src/modelMazeGame/Pemain.java
+++ MazeGame/src/modelMazeGame/Pemain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mazegameguidodika;
+package modelMazeGame;
 
 import java.awt.Image;
 import java.net.URL;
@@ -13,14 +13,20 @@ import javax.swing.ImageIcon;
  *
  * @author My Computer
  */
-public class Tembok extends Pixel {
-    public Tembok(int x, int y){
+public class Pemain extends Pixel {
+    public Pemain (int x, int y){
         super (x,y);
         
         URL location = this.getClass().getResource("");
         ImageIcon g = new ImageIcon(location);
         Image image = g.getImage();
         this.setImage(image);
-        
+    }
+    
+    public void Gerak(int x, int y){
+        int langkahX = this.getX() + x;
+        int langkahY = this.getY() + y;
+        this.setX(langkahX);
+        this.setY(langkahY);
     }
 }
