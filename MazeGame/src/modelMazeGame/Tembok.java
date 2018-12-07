66cf7f6b57cd859849a6999ca14cc7ae9275d37f
Lenovo <Lenovo@LAPTOP-8VJDIM5C>
2018-12-07 20:22:26 +0700

    coba2

--- MazeGame/src/modelMazeGame/Tembok.java
+++ MazeGame/src/modelMazeGame/Tembok.java
@@ -17,7 +17,7 @@ public class Tembok extends Pixel {
     public Tembok(int x, int y){
         super (x,y);
         
-        URL location = this.getClass().getResource("");
+        URL location = this.getClass().getResource("tembok.png");
         ImageIcon g = new ImageIcon(location);
         Image image = g.getImage();
         this.setImage(image);
